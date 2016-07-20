90908136c8fbe9a22035664094d6167d72375ac6
Erik Petersen <contacterikpetersen@gmail.com>
2016-07-20 15:25:53 -0400

    Added http call...does not work yet

--- assets/js/controllerChores.js
+++ assets/js/controllerChores.js
@@ -1,4 +1,4 @@
-mainApp.controller( "ControllerChores", [ "$scope", function( $scope ) {
+mainApp.controller( "ControllerChores", [ "$scope", "$http", function( $scope, $http ) {
 
   // list of all chores available
   // $scope.chores = [
@@ -13,6 +13,15 @@ mainApp.controller( "ControllerChores", [ "$scope", function( $scope ) {
   //  "assets/images/chores/vacuum-cleaner.png",
   //  "assets/images/chores/washing-machine-1.png"
   // ];
+
+  // $http({
+  //   method: 'GET',
+  //   url: 'http://a48f6bd3.ngrok.io/homes/1/chores/1.json',
+  // })
+  // .success( function(data) {
+  //   $scope.chores = data.data;
+  //   console.log( $scope.chores );
+  // });
 
   $scope.chores = [
     {
@@ -73,59 +82,3 @@ mainApp.controller( "ControllerChores", [ "$scope", function( $scope ) {
   }; // end removeChore()
 
 }]); // end ControllerChores
-
-
-
-
-// $scope.chores2 = [
-//   {
-//     "id": 0,
-//     "ChoreIcon": "assets/images/chores/bath.png",
-//     "ChoreName": "Clean bath"
-//   },
-//   {
-//     "id": 1,
-//     "ChoreIcon": "assets/images/chores/broom-1.png",
-//     "ChoreName": "Sweep floor"
-//   },
-//   {
-//     "id": 2,
-//     "ChoreIcon": "assets/images/chores/dishwasher.png",
-//     "ChoreName": "Load dishwasher"
-//   },
-//   {
-//     "id": 3,
-//     "ChoreIcon": "assets/images/chores/flower.png",
-//     "ChoreName": "Water flowers"
-//   },
-//   {
-//     "id": 4,
-//     "ChoreIcon": "assets/images/chores/liquid-soap.png",
-//     "ChoreName": "Wash dishes"
-//   },
-//   {
-//     "id": 5,
-//     "ChoreIcon": "assets/images/chores/plant.png",
-//     "ChoreName": "Water plants"
-//   },
-//   {
-//     "id": 6,
-//     "ChoreIcon": "assets/images/chores/recycle-bin.png",
-//     "ChoreName": "Empty recycling"
-//   },
-//   {
-//     "id": 7,
-//     "ChoreIcon": "assets/images/chores/sponge.png",
-//     "ChoreName": "Sponge walls"
-//   },
-//   {
-//     "id": 8,
-//     "ChoreIcon": "assets/images/chores/vacuum-cleaner.png",
-//     "ChoreName": "Vacuum ceiling"
-//   },
-//   {
-//     "id": 9,
-//     "ChoreIcon": "assets/images/chores/washing-machine-1.png",
-//     "ChoreName": "Do laundry"
-//   },
-// ];
