39855b5230c737c5a251e717357044b0df1dfca0
Travis Johnson <trav.johnson919@gmail.com>
2016-07-25 11:57:25 -0400

    monday stuff

--- assets/js/controllerChores.js
+++ assets/js/controllerChores.js
@@ -3,7 +3,7 @@ mainApp.controller( "ControllerChores", [ "$scope", "$http", function( $scope, $
   $scope.totalChores;
 
   $http({
-    url: 'https://d6c901d4.ngrok.io/homes/1/chores',
+    url: 'http://tiy-homeshare.herokuapp.com/homes/1/chores',
     method: 'GET',
     headers: {'Authorization':'maria@example.com'}
   }).success( function(data) {
